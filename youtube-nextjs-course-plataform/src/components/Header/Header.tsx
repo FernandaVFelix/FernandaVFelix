72742f4796aac04148b30131f554989efb82c94a
FernandaVFelix <felixfernandavictoria@gmail.com>
2025-11-12 17:15:45 -0300

    feat: improve header

--- youtube-nextjs-course-plataform/src/components/Header/Header.tsx
+++ youtube-nextjs-course-plataform/src/components/Header/Header.tsx
@@ -13,62 +13,38 @@ export const Header = () => {
     setTitle(document.title);
     setDrawer(false);
   }, [currentPath]);
+
+  useEffect(() => {
+    const handle = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setDrawer(false);
+      }
+    };
+
+    window.addEventListener("keydown", handle);
+    return () => window.removeEventListener("keydown", handle);
+  }, []);
+
   return (
-    <nav className="flex itens-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
-      <button className="sm:hidden" onClick={() => setDrawer(true)}>
-        <MdMenu size={24} />
-      </button>
+    <>
+      <nav className="flex itens-center gap-6 justify-start fixed top-0 right-0 left-0 md:justify-center bg-primary py-2 sm:py-4 px-6">
+        <button className="sm:hidden" onClick={() => setDrawer(true)}>
+          <MdMenu size={24} />
+        </button>
 
-      <ul
-        className="flex gap-4 items-center"
-        tabIndex={drawer ? -1 : undefined}
-      >
-        <li className="my-2">
-          <Link href={"/"} className="border-2 rounded-md py-2 px-1 font-bold">
-            CODARSE
-          </Link>
-        </li>
-        <li className="hidden sm:block">
-          <Link
-            href={"/"}
-            data-active={currentPath == "/"}
-            className="data-[active=true]:underline"
-          >
-            Página inicial
-          </Link>
-        </li>
-        <li className="hidden sm:block">
-          <Link
-            href={"/cursos"}
-            data-active={currentPath == "/cursos"}
-            className="data-[active=true]:underline"
-          >
-            Cursos
-          </Link>
-        </li>
-        <li className="hidden sm:block">
-          <Link
-            href={"http://blog.codarse.com"}
-            target="_blank"
-            className="flex gap-1 items-center"
-          >
-            Blog
-            <MdOpenInNew />
-          </Link>
-        </li>
-      </ul>
-
-      <div
-        data-open={drawer}
-        tabIndex={drawer ? undefined : -1}
-        onClick={() => setDrawer(false)}
-        className="bg-linear-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-full"
-      >
         <ul
-          className="flex gap-4 flex-col p-4 h-full w-60 bg-background "
-          onClick={(e) => e.stopPropagation()}
+          className="flex gap-4 items-center"
+          tabIndex={drawer ? -1 : undefined}
         >
-          <li className=" ">
+          <li className="my-2">
+            <Link
+              href={"/"}
+              className="border-2 rounded-md py-2 px-1 font-bold"
+            >
+              CODARSE
+            </Link>
+          </li>
+          <li className="hidden sm:block">
             <Link
               href={"/"}
               data-active={currentPath == "/"}
@@ -77,7 +53,7 @@ export const Header = () => {
               Página inicial
             </Link>
           </li>
-          <li className="">
+          <li className="hidden sm:block">
             <Link
               href={"/cursos"}
               data-active={currentPath == "/cursos"}
@@ -86,7 +62,7 @@ export const Header = () => {
               Cursos
             </Link>
           </li>
-          <li className="">
+          <li className="hidden sm:block">
             <Link
               href={"http://blog.codarse.com"}
               target="_blank"
@@ -97,9 +73,51 @@ export const Header = () => {
             </Link>
           </li>
         </ul>
-      </div>
 
-      <h1 className="sm:hidden py-2">{title}</h1>
-    </nav>
+        <div
+          data-open={drawer}
+          tabIndex={drawer ? undefined : -1}
+          onClick={() => setDrawer(false)}
+          className="sm:hidden bg-linear-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-full"
+        >
+          <ul
+            className="flex gap-4 flex-col p-4 h-full w-60 bg-background "
+            onClick={(e) => e.stopPropagation()}
+          >
+            <li className=" ">
+              <Link
+                href={"/"}
+                data-active={currentPath == "/"}
+                className="data-[active=true]:underline outline-offset-4"
+              >
+                Página inicial
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                href={"/cursos"}
+                data-active={currentPath == "/cursos"}
+                className="data-[active=true]:underline outline-offset-4"
+              >
+                Cursos
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                href={"http://blog.codarse.com"}
+                target="_blank"
+                className="flex gap-1 items-center outline-offset-4"
+              >
+                Blog
+                <MdOpenInNew />
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <h1 className="sm:hidden py-2 line-clamp-1">{title}</h1>
+      </nav>
+      <div className="h-14 sm:h-[72px]" />
+    </>
   );
 };
