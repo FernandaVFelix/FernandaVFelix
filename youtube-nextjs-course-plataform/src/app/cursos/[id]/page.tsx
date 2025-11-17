a045d194c11e3ba835ddca67f5d20413115027a4
FernandaVFelix <felixfernandavictoria@gmail.com>
2025-11-17 17:44:14 -0300

    feat: add courses detail page

---
+++ youtube-nextjs-course-plataform/src/app/cursos/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+interface Props {
+  params: { id: string };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: params.id,
+    description: params.id,
+  };
+}
+
+export default function PageCourseDetail({ params }: Props) {
+  return <main className="mt-8 flex justify-center">Detalhe {params.id}</main>;
+}
