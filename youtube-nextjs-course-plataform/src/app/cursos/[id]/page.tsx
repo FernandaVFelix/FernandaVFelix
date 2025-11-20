0ad4ae97a1954adb1ebe0a4812f3e0ded24c18e6
FernandaVFelix <felixfernandavictoria@gmail.com>
2025-11-20 15:00:47 -0300

    feat: add course header component

--- youtube-nextjs-course-plataform/src/app/cursos/[id]/page.tsx
+++ youtube-nextjs-course-plataform/src/app/cursos/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { CourseHeader } from "@/components/course-header/CourseHeader";
 import { Metadata } from "next";
 
 interface Props {
@@ -12,5 +13,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default function PageCourseDetail({ params }: Props) {
-  return <main className="mt-8 flex justify-center">Detalhe {params.id}</main>;
+  return (
+    <main className="mt-8 flex justify-center">
+      <div className="w-full min-[880px]:max-w-[880px]">
+        <CourseHeader />
+      </div>
+    </main>
+  );
 }
