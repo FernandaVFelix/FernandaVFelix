4d5cb7ba25b5babc5537886f2319f7d4dae8c2fd
FernandaVFelix <felixfernandavictoria@gmail.com>
2025-11-24 21:00:14 -0300

    feat: add startCourse floating button

--- youtube-nextjs-course-plataform/src/app/cursos/[id]/page.tsx
+++ youtube-nextjs-course-plataform/src/app/cursos/[id]/page.tsx
@@ -19,6 +19,7 @@ export default function PageCourseDetail({ params }: Props) {
       <div className="w-full min-[880px]:max-w-[880px] px-2 flex flex-col gap-4">
         <StartCourse
           idClass="1"
+          title="🎩 Curso de Figma para DEVs"
           idCourse="1"
           imageUrl="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
         />
--- youtube-nextjs-course-plataform/src/components/startCourse/StartCourse.tsx
+++ youtube-nextjs-course-plataform/src/components/startCourse/StartCourse.tsx
@@ -1,36 +1,57 @@
+"use client";
 import Link from "next/link";
 import { MdPlayCircleOutline } from "react-icons/md";
+import { useInView } from "react-intersection-observer";
 
 interface IStartCourseProps {
   idClass: string;
+  title: string;
   idCourse: string;
   imageUrl: string;
 }
 
 export const StartCourse = ({
   idClass,
+  title,
   idCourse,
   imageUrl,
 }: IStartCourseProps) => {
+  const [ref, inView] = useInView({ threshold: 0.2, initialInView: true });
+
   return (
-    <div className="p-3 bg-paper rounded-md flex flex-col gap-4">
-      <Link
-        href={`/player/${idCourse}/${idClass}`}
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-        className="w-full bg-cover bg-no-repeat aspect-video bg-center rounded"
-      >
-        <div className="w-full h-full flex items-center justify-center bg-background rounded opacity-0 hover:opacity-70 transition">
-          <MdPlayCircleOutline size={58} />
+    <>
+      <div ref={ref} className="p-3 bg-paper rounded-md flex flex-col gap-4">
+        <Link
+          href={`/player/${idCourse}/${idClass}`}
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+          className="w-full bg-cover bg-no-repeat aspect-video bg-center rounded"
+        >
+          <div className="w-full h-full flex items-center justify-center bg-background rounded opacity-0 hover:opacity-70 transition">
+            <MdPlayCircleOutline size={58} />
+          </div>
+        </Link>
+        <Link
+          href={`/player/${idCourse}/${idClass}`}
+          className="bg-primary p-2 px-3 rounded text-center"
+        >
+          Começar curso
+        </Link>
+      </div>
+
+      {!inView && (
+        <div className="p-3 px-2 bg-paper flex flex-col gap-4 absolute left-0 right-0 top-14">
+          <h1 className="font-extrabold text-xl">{title}</h1>
+
+          <Link
+            href={`/player/${idCourse}/${idClass}`}
+            className="bg-primary p-2 px-3 rounded text-center"
+          >
+            Começar curso
+          </Link>
         </div>
-      </Link>
-      <Link
-        href={`/player/${idCourse}/${idClass}`}
-        className="bg-primary p-2 px-3 rounded text-center"
-      >
-        Começar curso
-      </Link>
-    </div>
+      )}
+    </>
   );
 };
